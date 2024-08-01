efa1ed68bc3c58a819693be9e135ca035b539329
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-08-01 18:09:10 +0500

    quiz detail

--- kahoot/urls.py
+++ kahoot/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from kahoot import views
+
 urlpatterns = [
     path('', views.home_view, name='home-view'),
+    path('detail/', views.detail_view, name='detail-view')
 
 ]
--- kahoot/views.py
+++ kahoot/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'kahoot/home.html')
+
+
+def detail_view(request):
+    return render(request, 'kahoot/detail.html')
